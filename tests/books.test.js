a74be32fea912743a058a5b5c6e84e517862eeef
Marc Schär <marc.schaer93@gmail.com>
2024-02-18 09:42:05 +0100

    first commit

---
+++ tests/books.test.js
@@ -0,0 +1,91 @@
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+const db = require("../db");
+const app = require("../app");
+
+beforeAll(() => {
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+});
+
+// isbn of sample book
+let book_isbn;
+
+beforeEach(async () => {
+  let result = await db.query(
+    `
+    INSERT INTO 
+    books (isbn, amazon_url,author,language,pages,publisher,title,year)
+    VALUES(
+      '123456789',
+      'https://amazon.com/alchemist',
+      'Paulo',
+      'Coelho',
+      333,
+      'Nothing publishers',
+      'my first book', 2008)
+    RETURNING isbn`
+  );
+
+  book_isbn = result.rows[0].isbn;
+});
+
+describe("GET /books", function () {
+  test("Gets a list of books", async function () {
+    const response = await request(app).get("/books");
+    expect(response.body.books).toBeDefined();
+
+    const books = response.body.books;
+    expect(books).toHaveLength(1);
+    expect(books[0].isbn).toEqual("123456789");
+  });
+});
+
+describe("GET /books/:id", function () {
+  test("Get book by valid id", async function () {
+    const response = await request(app).get(`/books/${book_isbn}`);
+    expect(response.body.book).toBeDefined();
+
+    const book = response.body.book;
+    expect(book.author).toBe("Paulo");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("GET book by Invalid isbn", async function () {
+    const response = await request(app).get("/books/99999");
+    expect(response.body.book).toBeUndefined();
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("PUT /books/:id", function () {
+  test("Update book by id with given values", async function () {
+    const response = await request(app).put(`/books/${book_isbn}`).send({
+      amazon_url: "https://taco.com",
+      author: "mctest",
+      language: "english",
+      pages: 1000,
+      publisher: "yeah right",
+      title: "UPDATED BOOK",
+      year: 2000,
+    });
+    expect(response.body.book).toHaveProperty("isbn");
+    expect(response.body.book.title).toBe("UPDATED BOOK");
+  });
+});
+
+describe("DELETE /books/:id", function () {
+  test("Deletes a single book by id", async function () {
+    const response = await request(app).delete(`/books/${book_isbn}`);
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toBe("Book deleted");
+  });
+});
+
+afterEach(async function () {
+  await db.query("DELETE FROM BOOKS");
+});
+
+afterAll(async function () {
+  await db.end();
+});
